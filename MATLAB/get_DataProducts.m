2bae9d9dbd343f6929ba514696f687e5c1d043e4
Michael Hemming <m.hemming@unsw.edu.au>
2024-01-25 14:16:52 +1100

    Updated comments and improved code

--- MATLAB/get_DataProducts.m
+++ MATLAB/get_DataProducts.m
@@ -3,19 +3,24 @@
 % ####################################################################
 % 
 % Script: get_DataProducts.py
-% Description: Tutorial on how to download and 
-%                   load the AMDOT-EXT data products
+% Description: Tutorial on how to download and load the AMDOT-EXT data products
 % Created: 28 Nov 2022 by Michael Hemming (NSW-IMOS)
 % 
 % using Matlab version 9.8.0.1417392 (R2020a), 23.2.0.2409890 (R2023b) Update 3 
 % 
 % ######################################################################################################################
 % 
-% ####### Please cite the accompanying data set paper if you use the AMDOT-EXT data products #######
-% 
-% Hemming, M., Roughan, M., and Schaeffer, A.: Exploring Multi-decadal Time Series of Temperature Extremes in Australian Coastal Waters, 
-% Earth Syst. Sci. Data Discuss. (2024)
-% 
+% ####### Any and all use of the AMDOT-EXT data products or accompanying event summary CSV files described here must include ####### 
+%
+% – a citation to the data paper: "Exploring Multi-decadal Time Series of Temperature Extremes in Australian Coastal Waters: Hemming et al., (2024). 
+%    Earth System Science Data. https://doi.org/10.5194/essd-2023-252."
+%
+% – a reference to the data citation as written in the NetCDF file attributes and as follows: Hemming, MP. et al. (2023) "Australian Multi-decadal Ocean Time Series 
+%    EXTreme (AMDOT-EXT) Data Products", Australian Ocean Data Network, https://doi.org/10.26198/wbc7-8h24."
+%
+% – the following acknowledgement statement: Data were sourced from Australia’s Integrated Marine Observing System280
+% (IMOS) - IMOS is enabled by the National Collaborative Research Infrastructure Strategy (NCRIS).
+%
 % ######################################################################################################################
 % 
 % ##################################
@@ -24,49 +29,43 @@
 % Download the data products
  
 % The netCDF files are available for download here:
-% http://thredds.aodn.org.au/thredds/catalog/UNSW/UPDATE_HERE_WHEN_READY/catalog.html
+% https://thredds.aodn.org.au/thredds/catalog/UNSW/NRS_extremes/catalog.html
 
-% However, here we will load the file directly using OPeNDAP.
-
-% For more information on the files and methodology, please see CITATION
+% However, in this tutorial we will load the file directly using OPeNDAP.
 
 %% -----------------------------------------------------------
 % load the data products
 % We will use the Maria Island data product in this example
-
-path = 'C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\Temperature_extremes\Scripts\Tutorials\MATLAB\';
-
-% filename = 'http://thredds.aodn.org.au/thredds/catalog/UNSW/UPDATE_HERE_WHEN_READY/catalog.html'
-filename =['C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\Temperature_extremes\Data\', ...
-                'Finalised_data\MAI090_TEMP_EXTREMES_1944-2022_v1.nc'];
-
-cd(path)
-            
-% load file
-data = load_netCDF(filename,1);
+url = 'https://thredds.aodn.org.au/thredds/dodsC/UNSW/NRS_extremes/Temperature_DataProducts/MAI090/MAI090_TEMP_EXTREMES_1944-2022_v1.nc';
+data = load_netCDF(url,1)
 
 %% -----------------------------------------------------------
 % Export a selection of data as a spreadsheet
+% (Longest surface marine heatwave)
 
-% Using the 'MAI090_TEMP_EXTREMES_SUMMARY_1944-2022.xlsx' spreadsheet, find the longest surface marine heatwave. 
-% Info: event number 14, between 2016-01-16 00:00:00 and 2016-05-03 00:00:00
+% get times of the longest surface marine heatwave 
+SurfMHWs = data.TEMP_EXTREME_INDEX(1,:) == 12;
+SurfDuration = data.MHW_EVENT_DURATION(1,:); 
+LongestDuration = nanmax(SurfDuration(SurfMHWs));
+LongestSelection = SurfDuration == LongestDuration;
+LongestTimes = data.TIME(LongestSelection);
 
-% Select temperature and the 90th percentiles during the long 2016 marine heatwave
-c = data.TIME >= datenum(2016,01,16) & data.TIME <= datenum(2016,05,03);
+% Select temperature and the 90th percentiles during the longest surface marine heatwave
+c = data.TIME >= min(LongestTimes) & data.TIME <= max(LongestTimes);
 
 % time
-data_2016.TIME = datestr(data.TIME(c),'yyyy-mm-dd');
+data_selection.TIME = datestr(data.TIME(c),'yyyy-mm-dd');
 % temperature
-data_2016.TEMP_2m = data.TEMP(1,c)';
-data_2016.TEMP_21m = data.TEMP(2,c)';
+data_selection.TEMP_2m = data.TEMP(1,c)';
+data_selection.TEMP_21m = data.TEMP(2,c)';
 % 90th percentiles
-data_2016.PER90_2m = data.TEMP_PER90(1,c)';
-data_2016.PER90_21m = data.TEMP_PER90(2,c)';
+data_selection.PER90_2m = data.TEMP_PER90(1,c)';
+data_selection.PER90_21m = data.TEMP_PER90(2,c)';
 
 % export data as csv
-saving_path = ['C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\',... 
-               'Temperature_extremes\Scripts\Tutorials\MATLAB\']
-writetable(struct2table(data_2016), [saving_path,'MAI_TEMP_PER90_MHW14.csv'])
+% local saving path (uncomment and modify)
+% saving_path = ['local_path_to_save_the_CSV']
+writetable(struct2table(data_selection), [saving_path,'MAI_TEMP_PER90_LongestMHW.csv'])
 disp(['.csv saved in: ',saving_path])
 
 clear c
--- MATLAB/slice_DataProducts.m
+++ MATLAB/slice_DataProducts.m
@@ -3,49 +3,41 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Script: slice_DataProducts.py
-% Description: Tutorial on how to load the AMDOT-EXT data products
-%                   and slice data using event characteristics
+% Description: Tutorial on how to load the AMDOT-EXT data products and slice data using event characteristics
 % Created: 28 Nov 2022 by Michael Hemming (NSW-IMOS)
 
-% using Matlab version 9.8.0.1417392 (R2020a) 
+% using Matlab version 9.8.0.1417392 (R2020a), 23.2.0.2409890 (R2023b) Update 3 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%% Please cite the accompanying data set paper if you use the AMDOT-EXT data products %%%%%%%
-
-% ADD CITATION HERE
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ######################################################################################################################
+% 
+% ####### Any and all use of the AMDOT-EXT data products or accompanying event summary CSV files described here must include ####### 
+%
+% – a citation to the data paper: "Exploring Multi-decadal Time Series of Temperature Extremes in Australian Coastal Waters: Hemming et al., (2024). 
+%    Earth System Science Data. https://doi.org/10.5194/essd-2023-252."
+%
+% – a reference to the data citation as written in the NetCDF file attributes and as follows: Hemming, MP. et al. (2023) "Australian Multi-decadal Ocean Time Series 
+%    EXTreme (AMDOT-EXT) Data Products", Australian Ocean Data Network, https://doi.org/10.26198/wbc7-8h24."
+%
+% – the following acknowledgement statement: Data were sourced from Australia’s Integrated Marine Observing System280
+% (IMOS) - IMOS is enabled by the National Collaborative Research Infrastructure Strategy (NCRIS).
+%
+% ######################################################################################################################
+% 
+% ##################################
 
 %% -----------------------------------------------------------
 % Download the data products
 
 % The netCDF files are available for download here:
-% http://thredds.aodn.org.au/thredds/catalog/UNSW/UPDATE_HERE_WHEN_READY/catalog.html
-%
-% However, here we will load the file directly using OPeNDAP.
-%
-% For more information on the files and methodology, please see CITATION
+% https://thredds.aodn.org.au/thredds/catalog/UNSW/NRS_extremes/catalog.html
+
+% However, in this tutorial we will load the file directly using OPeNDAP.
 
 %% -----------------------------------------------------------
 % load the data products
-
 % We will use the Port Hacking data product in this example
-
-path = 'C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\Temperature_extremes\Scripts\Tutorials\MATLAB\';
-
-% filename = 'http://thredds.aodn.org.au/thredds/catalog/UNSW/UPDATE_HERE_WHEN_READY/catalog.html'
-filename =['C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\Temperature_extremes\Data\', ...
-                'Finalised_data\PH100_TEMP_EXTREMES_1953-2022_v1.nc'];
-
-cd(path)
-            
-% load file
-data = load_netCDF(filename,1);
-
-clear filename
+url = 'https://thredds.aodn.org.au/thredds/dodsC/UNSW/NRS_extremes/Temperature_DataProducts/PH100/PH100_TEMP_EXTREMES_1953-2022_v1.nc';
+data = load_netCDF(url,1)
 
 %% -----------------------------------------------------------
 % select data at 22m depth when there are strong MHWs
@@ -62,35 +54,31 @@ clear filename
 
 % get variable names in data structure
 vars = fieldnames(data);
-% convert data to cell for looping
-data_cell =  struct2cell(data);
 % select data during strong MHWs at 22m depth
-for n_var = 4:numel(vars)
-    data_loop = data_cell{n_var};
-    % select 22m depth
-    data_loop = data_loop(2,:);
-    % during strong MHWs
-    data_loop = data_loop(data.MHW_EVENT_CAT(2,:) == 2);
-    % save in new cell
-    data22mMHWStrong{n_var} = data_loop';
+for n_var = 1:numel(vars)
+    if isempty(strmatch('file_info',vars{n_var})) & isempty(strmatch('TIME',vars{n_var})) & isempty(strmatch('DEPTH',vars{n_var}))
+        data_loop = data.(vars{n_var});
+        % select 22m depth
+        data_loop = data_loop(2,:);
+        % during strong MHWs
+        data_loop = data_loop(data.MHW_EVENT_CAT(2,:) == 2);
+        % save in new cell
+        data22mMHWStrong.(vars{n_var}) = data_loop';
+    end
 end
-% convert from cell back to structure
-data22mMHWStrong = cell2struct(data22mMHWStrong,vars,2);
-% add time
+
+% add time (Will be the last column in the CSV file)
 data22mMHWStrong.TIME = datestr(data.TIME(data.MHW_EVENT_CAT(2,:) == 2),'yyyy-mm-dd');
-% remove unnecessary vars
-data22mMHWStrong = rmfield(data22mMHWStrong,'file_info');
-data22mMHWStrong = rmfield(data22mMHWStrong,'DEPTH');
 
 clear data_cell data_loop n_var vars
 
 %% -----------------------------------------------------------
 % calculate statistics and display
 
-IntMeanCumulative = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_CUMULATIVE),1)
-IntMeanMax = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_MAX),1)
-IntMean = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_MEAN),1)
-DurMean = round(nanmean(data22mMHWStrong.MHW_EVENT_DURATION))
+IntMeanCumulative = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_CUMULATIVE),1);
+IntMeanMax = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_MAX),1);
+IntMean = round(nanmean(data22mMHWStrong.MHW_EVENT_INTENSITY_MEAN),1);
+DurMean = round(nanmean(data22mMHWStrong.MHW_EVENT_DURATION));
 
 disp(['PH100 22m: Strong MHWs last on average ',num2str(DurMean),' days and have a mean intensity of ',num2str(IntMean), ...
       ' degrees celsius.'])
@@ -100,9 +88,8 @@ disp(['The average max and cumulative intensity is ', num2str(IntMeanMax),' degr
 %% -----------------------------------------------------------
 % save sliced dataset as NetCDF and CSV
 
-saving_path = ['C:\Users\mphem\OneDrive - UNSW\Work\Temperature_extremes\',... 
-               'Temperature_extremes\Scripts\Tutorials\MATLAB\'];
-
+% local saving path (uncomment and modify)
+% saving_path = ['local_path_to_save_the_CSV']
 % saving MAT file
 save([saving_path,'PH100_strong_MHWs_22m.mat'],'data22mMHWStrong');
 % export data as csv
