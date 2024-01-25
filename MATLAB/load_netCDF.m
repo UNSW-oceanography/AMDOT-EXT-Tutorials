23b447e179ca9a096cef48563264ec78234e0982
Michael Hemming <m.hemming@unsw.edu.au>
2024-01-25 14:17:36 +1100

    Modified to load OpeNDAP data products

--- MATLAB/load_netCDF.m
+++ MATLAB/load_netCDF.m
@@ -1,4 +1,4 @@
-function [data] = load_netCDF(filename,imos_time)
+function [data] = load_netCDF(opendapURL,imos_time)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -12,28 +12,36 @@ function [data] = load_netCDF(filename,imos_time)
 %
 % Input: 
 %
-% filename     |     string input including path and netCDF filename (including extension)
+% opendapURL     |     string input including opendap URL
 % imos_time   |     0/1 => no/yes to convert netCDF IMOS convention time (days since 1950-01-01) to MATLAB datenum 
 %
 % Output:
 %
 % data      |     structure file containing variables included in netCDF, plus file attributes and information
 
-%% get information
-data.file_info = ncinfo(filename);
+%% Open the NetCDF file
+ncid = netcdf.open(opendapURL, 'NOWRITE');
 
-%% Obtain all variables in file
+%% Get variable information
+info = ncinfo(opendapURL);
+variables = info.Variables;
 
-variables = data.file_info.Variables;
-
-% Loop through each variable and read its data
+%% Create a structure to store variable data and attributes
+data = struct();
+% Loop through each variable
 for i = 1:length(variables)
     varName = variables(i).Name;
-    data.(varName) = ncread(filename, varName);
-    
+    % Read variable data
+    data.(varName) = netcdf.getVar(ncid, netcdf.inqVarID(ncid, varName));
     % Display variable name and size
-    disp(['Variable: ', varName, ', Dimensions: ', num2str(size(varData))]);
+    disp(['Variable: ', varName, ', Dimensions: ', num2str(variables(i).Size)]);
 end
+
+%% store NetCDF file information
+data.file_info = info;
+
+%% Close the NetCDF file
+netcdf.close(ncid);
 
 %% convert from IMOS time to MATLAB datenum if required
 
