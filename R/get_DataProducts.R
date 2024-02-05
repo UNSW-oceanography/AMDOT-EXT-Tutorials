b83b68a919a3b9fa382a26dc9ca801c236ac3e4e
Michael Hemming <m.hemming@unsw.edu.au>
2024-02-05 13:38:49 +1100

    Modified R script to be consistent with MATLAB and Python versions

--- R/get_DataProducts.R
+++ R/get_DataProducts.R
@@ -62,10 +62,13 @@ varnames <- names(data[['var']])
 
 # get variables
 temp_extreme_index <- ncvar_get(data, "TEMP_EXTREME_INDEX")
+temp <- ncvar_get(data, "TEMP")
+temp_90 <- ncvar_get(data, "TEMP_PER90")
 mhw_event_duration <- ncvar_get(data, "MHW_EVENT_DURATION")
 # get TIME and convert to R time
 TIME <- data$dim$TIME[10]
 TIME_R <- as.POSIXct((as.numeric(unlist(TIME)) - 7305)*86400, origin = "1970-01-01", tz = "UTC")
+
 # Find surface marine heatwaves
 surf_mhws <- temp_extreme_index[1, ] == 12
 # Extract surface durations
@@ -74,65 +77,21 @@ surf_duration <- mhw_event_duration[1, ]
 longest_duration <- max(surf_duration[surf_mhws], na.rm = TRUE)
 # Identify the times with the longest duration
 longest_selection <- surf_duration == longest_duration
-longest_times <- TIME_R[longest_selection]
 
-# CONTINUE HERE
+# select data
+longest_times <- TIME_R[which(longest_selection)]
+longest_temp2m <- temp[1, which(longest_selection)]
+longest_temp21m <- temp[2, which(longest_selection)]
+longest_temp_90_2m <- temp_90[1, which(longest_selection)]
+longest_temp_90_21m <- temp_90[2, which(longest_selection)]
 
-#####################################################################################
-## THIS SEGMENT IS FROM CHATGPT BUT DOESN"T WORK PROPERLY
-
-# Select temperature and the 90th percentiles during the longest surface marine heatwave
-c <- complete.cases(longest_times) & data$TIME >= min(longest_times[!is.na(longest_times)]) & data$TIME <= max(longest_times[!is.na(longest_times)])
-
-
-# time
-data_selection$TIME <- format(data$TIME[c], "%Y-%m-%d")
-
-# temperature
-data_selection$TEMP_2m <- data$TEMP[1, c]
-data_selection$TEMP_21m <- data$TEMP[2, c]
-
-# 90th percentiles
-data_selection$PER90_2m <- data$TEMP_PER90[1, c]
-data_selection$PER90_21m <- data$TEMP_PER90[2, c]
-
-#####################################################################################
-
-
-
-# Using the 'MAI090_TEMP_EXTREMES_SUMMARY_1944-2022.xlsx' spreadsheet, find the longest surface marine heatwave. 
-# Info: event number 14, between 2016-01-16 00:00:00 and 2016-05-03 00:00:00
-
-# get TIME and convert to R time
-TIME <- data$dim$TIME[10]
-TIME_R <- as.POSIXct((as.numeric(unlist(TIME)) - 7305)*86400, origin = "1970-01-01", tz = "UTC")
-
-# surface
-# create dataframe including TIME
-data_surf <- data.frame(TIME = TIME_R)
-# add all other variables at the surface only
-for(i in 1:nvar) {
-  data_surf[varnames[i]] = c(ncvar_get(data, varnames[i],start=c(1,1),count=c(1,data$dim$TIME$len)))
-}
-
-# 21 m
-# create dataframe including TIME
-data_21m <- data.frame(TIME = TIME_R)
-# add all other variables at the surface only
-for(i in 1:nvar) {
-  data_21m[varnames[i]] = c(ncvar_get(data, varnames[i],start=c(2,1),count=c(1,data$dim$TIME$len)))
-}
-
-
-# Select temperature and the 90th percentiles during the long 2016 marine heatwave
-c <- as.Date(TIME_R) >= as.Date('2016-01-16') & as.Date(TIME_R) <= as.Date('2016-05-03')
-# time, temp, 90th percentiles
-data_2016 <- data.frame(TIME = TIME_R[c],
-                        TEMP_2m = data_surf$TEMP[c],
-                        TEMP_21m = data_21m$TEMP[c],
-                        PER90_2m = data_surf$TEMP_PER90[c],
-                        PER90_21m = data_21m$TEMP_PER90[c])
+# create dataframe
+data_longest <- data.frame(TIME = longest_times,
+                           TEMP_2m = longest_temp2m,
+                           TEMP_21m = longest_temp21m,
+                           PER90_2m = longest_temp_90_2m,
+                           PER90_21m = longest_temp_90_21m)
 
 # export data as csv
-saving_path <- 'C:\\Users\\z3526971\\OneDrive - UNSW\\Work\\Temperature_extremes\\Temperature_extremes\\Scripts\\Tutorials\\R\\'
-write.csv(data_2016, paste(saving_path,'MAI_TEMP_PER90_MHW14.csv'))
+# saving_path <- 'local\\path\\to\\save\\the\\CSV\\'
+# write.csv(data_longest, paste(saving_path,'MAI_TEMP_PER90_LongestMHW.csv'))
